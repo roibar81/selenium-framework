39c198f1c60adb7471f303c9315d1606fe1bdfde
roibar81 <baraziroi@gmail.com>
2023-06-06 19:19:36 +0300

    multiple windows handling, add functionality for catch the elements in each page.

--- src/test/java/widowshandle/MultiWindowsTest.java
+++ src/test/java/widowshandle/MultiWindowsTest.java
@@ -5,12 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
